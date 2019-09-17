5185b21240a9d8a5f5adab45baea531bef3ded3b
kirahan <hanzhaoapply@gmail.com>
2019-09-17 15:23:59 +0800

    add files

---
+++ src/components/Name3.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="playersresult">
+        <div class="rank">{{playerdata.rank}}</div>
+        <div class="name">{{playerdata.name}}</div>
+        <div class="result">{{playerdata.result}}</div>
+        <div class="t1">{{playerdata.t1}}</div>
+        <div class="t2">{{playerdata.t2}}</div>
+        <div class="t2">{{playerdata.t3}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props : ["playerdata"],
+    data(){
+        return {}
+    }
+}
+</script>
+
+<style lang="css">
+.playersresult{
+    display : flex;
+    color: white;
+    font-size: 30px;
+}
+.rank{
+    width: 10%;
+    background-color: burlywood;
+}
+.name{
+    width: 30%;
+     background-color:cornflowerblue
+}
+.result{
+    width: 20%;
+     background-color :darkcyan
+}
+.t1{
+    width: 20%;
+     background-color :darkslateblue
+}
+.t2{
+    width: 20%;
+    background-color :darkslateblue
+}
+.t3{
+    width: 20%;
+    background-color :darkslateblue
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,68 @@
   <div class="home">
       <h2>这是展示页面</h2>
       <p>我们要在这里写界面呈现效果</p>
+    
+      <Playersresult3  v-for="(data,i) in resultdata" :key="i" :playerdata='data'></Playersresult3>
+     
+      
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import Playersresult3 from '../components/Name3'
 export default {
   name: 'home',
+  components: { Playersresult3 },
+  data(){
+    return {
+        resultdata : [
+          {
+            rank : 1,
+            name : 'hanzhao',
+            result : 21.35,
+            t1:15.36,
+            t2:25.78,
+            t3:26.35
+          },
+          {
+            rank : 2,
+            name : 'nzhao',
+            result : 25.35,
+            t1:25.36,
+            t2:25.78,
+            t3:36.35
+          }
+          ,
+          {
+            rank : 3,
+            name : 'zhao',
+            result : 32.35,
+            t1:15.36,
+            t2:25.78,
+            t3:26.35
+          }
+          ,
+          {
+            rank : 4,
+            name : 'jo',
+            result : 44.35,
+            t1:15.36,
+            t2:25.78,
+            t3:26.35
+          }
+          ,
+          {
+            rank : 5,
+            name : 'kira',
+            result : 52.35,
+            t1:15.36,
+            t2:25.78,
+            t3:26.35
+          }
+        ]
+    }
+  }
 }
 </script>
