6c0d313c39563df68a5428fc615949a65c10a683
kirahan <hanzhaoapply@gmail.com>
2019-09-27 23:21:43 +0800

    fix bugs

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,15 @@
-#!/usr/bin/env sh
-
-# 当发生错误时中止脚本
+# deploy.sh
+# 错误时停止
 set -e
-
-# 构建
+# 打包
 npm run build
-
-# cd 到构建输出的目录下 
+# 进入目标文件夹
 cd dist
 
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
+# 提交到本地仓库
 git init
 git add -A
-git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
+git commit -m "deploy"
 git push -f git@github.com:kirahan/CubeResultDisplayTools.git master:gh-pages
 
 cd ..
